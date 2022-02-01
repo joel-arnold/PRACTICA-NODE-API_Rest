e0cec18ebeec786cb3982e30611da6c8b7ec1032
Joel Arnold <ar.joelarnold@yahoo.com.ar>
2022-01-31 23:25:17 -0300

    sadasdasd

--- models/server.js
+++ models/server.js
@@ -6,15 +6,16 @@ const { dbConnection } = require('../database/config');
 class Server {
 
     constructor() {
-        this.app  = express();
+        this.app = express();
         this.port = process.env.PORT;
 
+        // TODO No usado por completo
         this.paths = {
-            auth:       '/api/auth',
-            buscar:     '/api/buscar',
+            auth: '/api/auth',
+            buscar: '/api/buscar',
             categorias: '/api/categorias',
-            productos:  '/api/productos',
-            usuarios:   '/api/usuarios',
+            productos: '/api/productos',
+            usuarios: '/api/usuarios',
         }
 
 
@@ -36,28 +37,28 @@ class Server {
     middlewares() {
 
         // CORS
-        this.app.use( cors() );
+        this.app.use(cors());
 
         // Lectura y parseo del body
-        this.app.use( express.json() );
+        this.app.use(express.json());
 
-        // Directorio Público
-        this.app.use( express.static('public') );
+        // ! Directorio Público
+        this.app.use(express.static('public'));
 
     }
 
     routes() {
-        
-        this.app.use( this.paths.auth, require('../routes/auth'));
-        this.app.use( this.paths.buscar, require('../routes/buscar'));
-        this.app.use( this.paths.categorias, require('../routes/categorias'));
-        this.app.use( this.paths.productos, require('../routes/productos'));
-        this.app.use( this.paths.usuarios, require('../routes/usuarios'));
+
+        this.app.use(this.paths.auth, require('../routes/auth'));
+        this.app.use(this.paths.buscar, require('../routes/buscar'));
+        this.app.use(this.paths.categorias, require('../routes/categorias'));
+        this.app.use(this.paths.productos, require('../routes/productos'));
+        this.app.use(this.paths.usuarios, require('../routes/usuarios'));
     }
 
     listen() {
-        this.app.listen( this.port, () => {
-            console.log('Servidor corriendo en puerto', this.port );
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port);
         });
     }
 
